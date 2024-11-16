e311f6b07a7af33e949f1f462030624ebed6a071
Arturo C-Zacarias <accesareo@gmail.com>
2024-11-16 11:14:29 -0800

    Transfered scripts from door-functionality branch

---
+++ Assets/Scripts/Door/DoorGroup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorGroup : MonoBehaviour
+{
+    [Range(1, 4)]
+    public int buildingNum;
+    public bool isUnlocked;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isUnlocked = !isUnlocked;
+            //Debug.Log("is open: " + isUnlocked);
+        }
+    }
+
+    public bool TryUnlock()
+    {
+        bool playerHasCard = KeyCardManager.instance.hasCard(buildingNum);
+        isUnlocked = playerHasCard;
+        return playerHasCard;
+    }
+
+}
